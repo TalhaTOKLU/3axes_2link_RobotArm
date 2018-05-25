ee8d47b4a21909c2adde624ebd33aa8c990adf8f
TalhaTOKLU <36067617+TalhaTOKLU@users.noreply.github.com>
2018-05-25 05:03:26 +0300

    Delete rotation.m

--- rotation.m
+++
@@ -1,8 +0,0 @@
-function rot = rotation(th)
-% linki origine göre th ile döndür
-% giriþ deðerleri th
-% Final_transformation tarafýndan çaðýrýlýyor
-
-rot = [cos(th) -sin(th) 0 ;
-       sin(th) cos(th)  0 ;
-       0        0       1 ];
